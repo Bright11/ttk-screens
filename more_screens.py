ac99f6dbad6dedf19c82954f3765ff683dd927e6
Bright11 <chikanwazuo@gmail.com>
2024-02-09 23:43:40 -0800

    new screen

--- more_screens.py
+++ more_screens.py
@@ -17,8 +17,13 @@ left_frame = Frame(second_frame, width=700, height=300, bg="pink")
 left_frame.grid(row=0, column=0, padx=10, pady=10, sticky="nsew")
 
 # Right frame for calculator display and buttons
-right_frame = Frame(second_frame, width=1100, height=300, bg="green")
+right_frame = Frame(second_frame, width=550, height=300, bg="green")
 right_frame.grid(row=0, column=1, padx=10, pady=10, sticky="nsew")
+second_frame.columnconfigure(1, weight=1)  # Set column 1 to expand with window size
+
+
+right_frame2 = Frame(second_frame, width=550, height=300, bg="blue")
+right_frame2.grid(row=0, column=2, padx=10, pady=10, sticky="nsew")
 
 
 # Label for calculator display
